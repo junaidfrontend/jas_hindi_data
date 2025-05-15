ed6d097b4d0c4ed43958027a1a67a159a4339138
junaidfrontend <junaid@nursingnextlive.in>
2025-05-15 12:16:14 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "junaid@nextlive.com"
+var accountPassword = "123456"
+accountCity = "New Delhi"
+let accountState;
+
+// accountId = 2 // not allowed
+
+console.log(accountId);
+
+accountEmail = "hc@hc.in"
+accountPassword = "212121"
+accountCity = "Shamli"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
